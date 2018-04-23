25d816875393d2c14498e88a23aa83a79055603d
shashijeevan <shashijeevan@gmail.com>
2018-04-23 12:10:38 +0000

    Changed the prediction engine to use Keras and CNTK trained models. Need to refactor code and add the training code.

--- cgi-bin/mnist.py
+++ cgi-bin/mnist.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 CGI script that accepts image urls and feeds them into a ML classifier. Results
 are returned in JSON format. 
@@ -11,7 +12,11 @@ import re
 import base64
 import numpy as np
 from PIL import Image
-from model import model
+#from model import model
+
+import keras
+from keras.models import model_from_json
+
 
 # Default output
 res = {"result": 0,
@@ -32,15 +37,34 @@ try:
         # Normalize and invert pixel values
         arr = (255 - arr) / 255.
 
+        img_rows, img_cols = 28, 28
+
         # Load trained model
-        model.load('cgi-bin/models/model.tfl')
+#        model.load('cgi-bin/models/model.tfl')
 
         # Predict class
-        predictions = model.predict([arr])[0]
+#        predictions = model.predict([arr])[0]
+
+#Using saved model and weights for prediction
+
+# load json and create model
+        json_file = open('cgi-bin/models/model.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+        loaded_model.load_weights("cgi-bin/models/model.h5")
+
+        loaded_model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+        test_img = arr.reshape((1,img_rows,img_cols,1))
+        res['data'] = [float(num) for num in loaded_model.predict(test_img)[0]]
 
         # Return label data
         res['result'] = 1
-        res['data'] = [float(num) for num in predictions] 
+#        res['data'] = [float(num) for num in predictions] 
 
 except Exception as e:
     # Return error data
